4dc19da8117bd2e9f9bf838680c9959d1aed2423
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-10-10 22:00:38 +0600

    add boostrap form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import './App.css';
-import { getAuth } from "firebase/auth"
-import app from './firebase.init';
-
-const auth = getAuth(app)
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function App() {
-
-
-
-
-
   return (
-    <div className="App">
+    <div>
+      <div className="registration w-50 mx-auto mt-5">
+
+        <h2 className='text-primary'>Please Register</h2>
+
+
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Already registered?" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+
 
     </div>
   );
